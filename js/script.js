a6dd80c75e9f029c0635dd70ec02c6d4873d88bc
mttpisi <matteo.pisi@gmail.com>
2019-05-02 09:17:53 +0200

    aggiunta del prefisso telefonico sotto forma di select

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-document.getElementById("inputTelefono").addEventListener("keydown",function(){
-    var charCode = (event.which) ? event.which : event.keyCode;
-    console.log(charCode)
-    if((charCode < 48 || charCode > 57) && charCode != 8 && charCode != 37 && charCode != 39){
-        event.preventDefault();
-    }
-})
+
 
 document.getElementById("inputCheck").addEventListener("invalid", function(){
     this.setCustomValidity("Assicurati di aver letto l'informativa sulla privacy")
